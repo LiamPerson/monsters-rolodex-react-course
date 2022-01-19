9c80e68cc28204bdb71d6283d1ae9936b4f9fc44
Liam <yelopartyhat@gmail.com>
2022-01-19 17:53:15 +1000

    Attempted fix at publish through gh-pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.styles.css';
 export const Card = (props) => {
     return (
     <div className="card-container">
-        <img alt="monster" width='100%' height='180px' src={ `/imgs/${props.monster.id}.jpg` /*`https://robohash.org/${props.monster.id}?set=set2&size=180x180`*/} />
+        <img alt="monster" width='100%' height='180px' src={ `/monsters-rolodex-react-course/imgs/${props.monster.id}.jpg` /*`https://robohash.org/${props.monster.id}?set=set2&size=180x180`*/} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
